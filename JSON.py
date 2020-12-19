534f2fabd0f3c75808ce5d447997869440a71fb3
alexandersidorevitch <alexandersidorevitch@yandex.ru>
2020-12-19 18:10:23 +0300

    Настройки для Deploy

--- JSON.py
+++ JSON.py
@@ -2,10 +2,10 @@ import json
 
 
 def read_from_json(file_name: str) -> dict:
-    with open(file_name, 'r') as file:
+    with open(file_name, 'r', encoding='ISO-8859-1') as file:
         return json.load(file)
 
 
 def write_to_json(file_name: str, data: dict) -> None:
-    with open(file_name, 'w') as file:
+    with open(file_name, 'w', encoding='ISO-8859-1') as file:
         json.dump(data, file, ensure_ascii=False, indent=4)
