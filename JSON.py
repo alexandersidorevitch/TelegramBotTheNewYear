a92e3eb284d6875c80c6e0aac3f40639feedd704
alexandersidorevitch <alexandersidorevitch@yandex.ru>
2020-12-19 18:31:17 +0300

    Исправления проблем с кодировкой

--- JSON.py
+++ JSON.py
@@ -2,10 +2,10 @@ import json
 
 
 def read_from_json(file_name: str) -> dict:
-    with open(file_name, 'r', encoding='ISO-8859-1') as file:
+    with open(file_name, 'r', encoding='UTF-8') as file:
         return json.load(file)
 
 
 def write_to_json(file_name: str, data: dict) -> None:
-    with open(file_name, 'w', encoding='ISO-8859-1') as file:
+    with open(file_name, 'w', encoding='UTF-8') as file:
         json.dump(data, file, ensure_ascii=False, indent=4)
