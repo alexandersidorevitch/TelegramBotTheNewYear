568ac6a208b142bb88922913f637cffd2c1b63ea
alexandersidorevitch <alexandersidorevitch@yandex.ru>
2020-12-19 17:55:47 +0300

    Версия бота 1/0V.

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+import random
+
 import telebot
 
 import JSON
@@ -9,7 +11,7 @@ users_json = 'users.json'
 
 def create_user(data, message):
     data[message.from_user.id] = {'name': message.from_user.first_name, 'username': message.from_user.username,
-                                  'last_name': message.from_user.last_name, 'wantable_presents': []}
+                                  'last_name': message.from_user.last_name, 'wantable_presents': [], 'recipient': None}
     JSON.write_to_json(users_json, data)
 
 
@@ -20,20 +22,32 @@ def update_presents(data, message):
     data[str(message.from_user.id)]['wantable_presents'] = list(set(urls))
 
 
+def send_to_everyone(users_id, message: str) -> None:
+    for ID in users_id:
+        try:
+            bot.send_message(ID, message)
+        except:
+            pass
+
+
 @bot.message_handler(commands=['start'])
 def start_message(message):
     data = JSON.read_from_json(users_json)
     if data.get(str(message.from_user.id), None) is not None:
         bot.send_message(message.chat.id,
-                         f'Привет, {message.chat.first_name if message.chat.first_name else ""} {message.chat.last_name if message.chat.last_name else ""}, а я тебя знаю!')
+                         f'{normalize_string(message.chat.first_name)} {normalize_string(message.chat.last_name)}, а я тебя знаю уже!')
     else:
         create_user(data, message)
         bot.send_message(message.chat.id,
-                         f'Привет, {message.chat.first_name if message.chat.first_name else ""} {message.chat.last_name if message.chat.last_name else ""}, я тебя добавил в список гостей')
+                         f'Привет, {normalize_string(message.chat.first_name)} {normalize_string(message.chat.last_name)}, я тебя добавил в список гостей')
+        bot.send_message(message.chat.id, 'Для этого, присылайте дедушке, что бы Вы хотели на Новый Год.')
+        bot.send_message(message.chat.id, 'Дедушка молодой, прогрессивный, так что присылайте дедушке ссылки на '
+                                          'подарки, так дедушке будет легче разобраться. '
+                                          'Можете присылать по одной или несколько ссылок. Я все запишу))')
 
 
-@bot.message_handler(commands=['news'])
-def news(message):
+@bot.message_handler(regexp=r'Рассылка.*')
+def mailing(message):
     if str(message.from_user.id) != '531184087':
         return
     bot.send_message(message.chat.id,
@@ -43,8 +57,12 @@ def news(message):
 
 
 def print_news(message):
-    for message_id in JSON.read_from_json(users_json):
-        bot.send_message(message_id, message.text)
+    send_to_everyone(JSON.read_from_json(users_json), message.text)
+
+
+def normalize_string(s: str) -> str:
+    return s if s else ''
+
 
 @bot.message_handler(
     regexp=r"(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\/=]*))+")
@@ -52,14 +70,34 @@ def set_presents(message):
     data = JSON.read_from_json(users_json)
     if data.get(str(message.from_user.id), None) is not None:
         update_presents(data, message)
-        bot.send_message(message.chat.id,
-                         f'Привет, {message.chat.first_name if message.chat.first_name else ""} {message.chat.last_name if message.chat.last_name else ""}, а я тебя знаю!')
     else:
         create_user(data, message)
         update_presents(data, message)
         bot.send_message(message.chat.id,
-                         f'Привет, {message.chat.first_name if message.chat.first_name else ""} {message.chat.last_name if message.chat.last_name else ""}, я тебя добавил в список гостей')
+                         f'Привет, {normalize_string(message.chat.first_name)} {normalize_string(message.chat.last_name)}, я тебя добавил в список гостей')
     bot.reply_to(message, 'Добавил Ваши желание в свой список))')
+    JSON.write_to_json(users_json, data)
+
+
+@bot.message_handler(regexp=r'НГ.*')
+def the_new_year(message):
+    if str(message.from_user.id) != '531184087':
+        return
+    data = JSON.read_from_json(users_json)
+    users = list(data.keys())
+    random.shuffle(users)
+    for i, ID in enumerate(users):
+        data[ID]['recipient'] = users[(i + 1) % len(users)]
+
+    data_keys = list(data.keys())
+    for ID in data_keys:
+        try:
+            bot.send_message(ID,
+                             f"Ты даришь подарок человечку с именем {normalize_string(data[data[ID]['recipient']]['name'])} {normalize_string(data[data[ID]['recipient']]['last_name'])}: ")
+            for present in data[data[ID]['recipient']]['wantable_presents']:
+                bot.send_message(ID, f'Он(а) хочет {present}')
+        except:
+            del data[ID]
     JSON.write_to_json(users_json, data)
 
 
